2a5d4761acb0e86bc5cdda8c89aaba60f4958e94
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-06 18:57:52 -0700

    My fifth print commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Start point
+  *
+  * Return: Always 0 (Ends))
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
