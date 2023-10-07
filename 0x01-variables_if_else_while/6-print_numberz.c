b851504bd583dbba7aa2d631cbf20aaa9966f77e
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-06 19:00:59 -0700

    My sixth print commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Start point
+  *
+  * Return: Always 0 (Ends)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
