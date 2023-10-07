020a66801579883a3cae9c5fd25525862959dd19
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-06 18:44:31 -0700

    My print commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Start point
  *
- * Return: Always 0 (End)
+ * Return: Always (Ends)
  */
-
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
+	putchar('\n');
 	return (0);
 }
