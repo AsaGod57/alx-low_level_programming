7821540e941b6dbff3f79eb69e232aa41bfcfb2a
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-06 19:24:01 -0700

    My second print combo commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  * main - Start point
+  *
+  * Return: Always 0 (Ends)
+  */
+int main(void)
+{
+	int j,k,l;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		for (k = '0'; k <= '9'; k++)
+		{
+			for (l = '0'; l <= '9'; l++)
+			{
+				if (j < k && k < l)
+				{
+					putchar(j);
+					putchar(k);
+					putchar(l);
+
+					if (j != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
