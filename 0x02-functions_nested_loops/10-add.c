c2a8ec5ed12dfb356b6c050bf92e50ba9f25f3a5
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-10 22:00:44 -0700

    My first add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Start point
+  * @n1: Specifies the first number to be computed
+  * @n2: Specifies the second number to be computed
+  *
+  * Return: Integer value (Ends)
+  */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
