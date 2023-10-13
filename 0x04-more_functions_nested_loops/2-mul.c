dc344fd0a33f90ae7c9f7b65fed4389a0d645e46
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-12 17:11:13 -0700

    My multiplication commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - Start point
+  * @x: Specifies the first number to multiply
+  * @y: Specifies the second number to multiply with first number
+  *
+  * Return: The result of multiplication between x param and y param (Ends)
+  */
+int mul(int x, int y)
+{
+	return (x * y);
+}
