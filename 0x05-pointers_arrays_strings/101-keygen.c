7b5c4cb8c50df09a64ba9a51e8c8ab34a5a8a704
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-16 22:05:57 -0700

    My keygen commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /** main - Start point
- * Keygen Software : 101-Crackme
- * Return : Void (Ends)
+ * Keygen Software: Based on 101-crackme
+ * Return : Void (Ends) 
  */
-int main (void)
+int main(void)
 {
-	char password [84]
-	int index = 0, sum = 0, diff_half1, diff_half2;
-	srand (time(0))	
-	while (sum < 2772)
-{
-	password [index]=33+rand()%94;
-	sum += password [index++);
-	password [index] = '10';
-	if (sum ! = 2772)
-		diff_half1 = (sum - 2772);
-		diff_half2 = (sum - 2772);
-	if ((sum - 2772) % 2! = 0)
-		diff_half1++
-	for (index = 0; password [index]; index++)
+    char password[84];
+    int index = 0, sum = 0, diff_half1, diff_half2;
+    srand(time(0));
 
-	{
-	if (password [index] >= (33 + diff_half1))
-		password -= diff_half1;
-	break;
-	}
-	for (index = 0; password[index]; index ++)
-	{
-		if (password [index] >= (33 + diff_half2))
-			password [index] -= diff_half2;
-		break;
-	}
+    while (sum < 2772)
+    {
+        password[index] = 33 + rand() % 94;
+        sum += password[index];
+        index++;
+    }
 
+    diff_half1 = (sum - 2772);
+    diff_half2 = (sum - 2772);
+
+    if ((sum - 2772) % 2 != 0)
+    {
+        diff_half1++;
+    }
+
+    for (index = 0; password[index]; index++)
+    {
+        if (password[index] >= (33 + diff_half1))
+        {
+            password[index] -= diff_half1;
+        }
+    }
+
+    for (index = 0; password[index]; index++)
+    {
+        if (password[index] >= (33 + diff_half2))
+        {
+            password[index] -= diff_half2;
+        }
+    }
+
+    printf("%s", password);
+    return 0;
 }
-	printf("%s",password);
-	return (0);
-}
-
-
 
