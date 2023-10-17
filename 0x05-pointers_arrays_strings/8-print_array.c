f165e8bfaa5d427d5252aef7624d0e1a605d1c84
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-16 19:34:19 -0700

    My print array commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * print_array - Start point
@@ -8,17 +9,13 @@
   */
 void print_array(int *a, int n)
 {
-	int j;
+	int i = 0;
 
-	for (j = 0; j < n; j++)
+	for (; i < n; i++)
 	{
-		printf("%d", a[j]);
-
-		if (j != (n - 1))
-		{
+		printf("%d", a[i]);
+		if (i < n - 1)
 			printf(", ");
-		}
 	}
-
-	printf("\n");
+	putchar('\n');
 }
