2aed66edc341b14e5d0ddfe8d637154134fff91a
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-19 22:48:50 -0700

    My magic commit

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _magic - Start point
+ * @n - Specifies the value
+ * Return: Void (Ends)
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
