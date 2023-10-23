a46080e2229ac5d11a7377d42971d1ce21ae7975
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-23 16:22:05 -0700

    My memeset commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "Main.h"
+
+/**
+  * _memset - Start point
+  * @s: Specifies memory area to fill
+  * @b: Specifies constant byte to fill
+  * @n: Specifies bytes of memory area to fill
+  * Return: Memory area filled (Ends)
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
