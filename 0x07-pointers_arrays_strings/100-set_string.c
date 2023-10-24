f7ce3bb7658da844bdad7618de07ea5c59440946
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-23 18:46:04 -0700

    My string set commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * set_string - Start point
+  * @s: Specifies the value to modify
+  * @to: Specifies the value to assign
+  * Return: Void (Ends)
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
