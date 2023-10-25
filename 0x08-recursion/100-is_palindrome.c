64316d405646b69fc6ee9a07b150baf1ffbfd683
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-25 09:39:56 -0700

    My palindrome commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,55 +1,53 @@
 #include "main.h"
 #include <stdio.h>
 
-int check_palindrome(char *s);
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
 
 /**
-  * is_palindrome - Start point
-  * @s: Specifies the string value to be checked
-  * Return: The integer value (Ends)
-  */
-int is_palindrome(char *s)
-{
-	if (*s == '0')
-		return (1);
-
-	return (check_palindrome(s));
-}
-
-/**
-  * check_palindrome - Start point
-  * @s: Specifies the string value to be checked
-  * Return: The integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
-  * _strlen_recursion - Start point
-  * @s: Specifies the string to get the length
-  * Return: The string length (Ends)
-  */
+ *_strlen_recursion - Start point
+ * @s: Specifies the string to calculate length
+ * Return: The length of string (Ends)
+ */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (!*s)
 	{
 		return (0);
 	}
+	return (_strlen_recursion(s + 1) + 1);
+}
 
-	s++;
-	return (_strlen_recursion(s) + 1);
+/**
+ * is_palindrome - Start point
+ * @s: Specifies the inpuit string
+ * Return: 1 if is string or 0 if composite (Ends)
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
+}
+
+/**
+ * helper_palindrome - Start point 
+ * @s: Specifes the input string
+ * @len: Specifies the length of the string
+ * Return: The reverse string (Ends)
+ */
+int helper_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (helper_palindrome(s + 1, len - 2));
+	}
+	else
+		return (0);
+
 }
