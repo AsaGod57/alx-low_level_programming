282dd13680bfb283f296d9093dacdf885b9b26f1
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-27 21:14:53 -0700

    My change commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Start point
+ * @argc: Specifies number of arguments
+ * @argv: Specifies the array with the arguments
+ * Return: 0 succesfull exit (Ends)
+ **/
+
+int main(int argc, char *argv[])
+{
+	int dev = 0, coins = 0;
+	char c[] = "Error";
+
+	if (argc != 2)
+	{
+		printf("%s\n", c);
+		return (1);
+	}
+	dev = atoi(argv[1]);
+	while (dev >= 25)
+	{
+		dev -= 25;
+		coins++;
+	}
+	while (dev >= 10)
+	{
+		dev -= 10;
+		coins++;
+	}
+	while (dev >= 5)
+	{
+		dev -= 5;
+		coins++;
+	}
+	while (dev >= 2)
+	{
+		dev -= 2;
+		coins++;
+	}
+	if (dev == 1)
+		coins++;
+	printf("%d\n", coins);
+	return (0);
+}
