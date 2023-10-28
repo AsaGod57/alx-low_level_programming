2abcf8a5953ecd2052c61ac76ede19afab8c09fb
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-27 19:26:25 -0700

    My whatsmyname commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Start point
+ * @argc: Specifies the argument count
+ * @argv: Specifies the array of argument strings
+ * Return: 0 for succesful exit (Ends)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef Main_H
+#define Main_H
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+#endif /* Main_H */
