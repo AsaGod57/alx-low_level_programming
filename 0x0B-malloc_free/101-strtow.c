e842bb48cc8afcc2a475ae4a43a8d450253b8723
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-31 18:10:21 -0700

    My strtow commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,85 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * number - Start point
- * @str: Specifies the string being passed to check for words
- * Return: The number of words (Ends)
+ * _strcmp - Start point
+ * @s1: Specifies the first string
+ * @s2: Specifies the second string
+ * Return: Nothing (Ends)
  */
-int number(char *str)
+int _strcmp(char *s1, char *s2)
 {
-	int a, num = 0;
+	int i = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*str == ' ')
-			str++;
-		else
+		if (s1[i] != s2[i])
 		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
-	return (num);
-}
-/**
- * free_everything - Start point
- * @string: Specifies the pointer values being passed for freeing
- * @i: Specifies the counter
- */
-void free_everything(char **string, int i)
-{
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
-}
-
-/**
- * strtow - Start point
- * @str: Specifies the string being passed
- * Return: NULL if string is empty
- */
-char **strtow(char *str)
-{
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
-
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-			b++; c = 0; length = 0; str++;
-		}
-	}
-	return (words);
+	return (0);
 }
