70db56b9f64493bf07b3dfc6c2b06bb526f26ee8
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-31 17:13:39 -0700

    My allocation grid commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Start point
+ * @width: Specifies the int 1
+ * @height: Specifies the int 2
+ * Return: The pointer (Ends)
+ */
+int **alloc_grid(int width, int height)
+{
+	int **_new_height;
+	int i = 0, j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	_new_height = malloc(height * sizeof(int *));
+	if (_new_height == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		_new_height[i] = malloc(width * sizeof(int));
+		if (_new_height[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(_new_height[i]);
+			}
+			free(_new_height);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			_new_height[i][j] = 0;
+		}
+	}
+	return (_new_height);
+}
