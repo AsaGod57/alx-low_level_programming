17490adf2559b1bd1dc58b106f2ddc9a6d8ac858
Godsway <gasamoah@wacci.ug.edu.gh>
2023-10-31 20:01:17 -0700

    My strtow commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,23 +3,84 @@
 #include <stdio.h>
 
 /**
- * _strcmp - Start point
- * @s1: Specifies the first string
- * @s2: Specifies the second string
- * Return: Nothing (Ends)
+ * number - Start point
+ * @str: Specifis the string to check for words
+ * Return: The number of words
  */
-int _strcmp(char *s1, char *s2)
+int number(char *str)
 {
-	int i = 0;
+	int a, num = 0;
 
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (a = 0; str[a] != '\0'; a++)
 	{
-		if (s1[i] != s2[i])
+		if (*str == ' ')
+			str++;
+		else
 		{
-			return (s1[i] - s2[i]);
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			num++;
 		}
-		i++;
 	}
-	return (0);
+	return (num);
+}
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
+char **strtow(char *str)
+{
+	int total_words = 0, b = 0, c = 0, length = 0;
+	char **words, *found_word;
+
+	if (str == 0 || *str == 0)
+		return (NULL);
+	total_words = number(str);
+	if (total_words == 0)
+		return (NULL);
+	words = malloc((total_words + 1) * sizeof(char *));
+	if (words == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < total_words;)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			found_word = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+				length++;
+				str++;
+			}
+			words[b] = malloc((length + 1) * sizeof(char));
+			if (words[b] == 0)
+			{
+				free_everything(words, b);
+				return (NULL);
+			}
+			while (*found_word != ' ' && *found_word != '\0')
+			{
+				words[b][c] = *found_word;
+				found_word++;
+				c++;
+			}
+			words[b][c] = '\0';
+			b++; c = 0; length = 0; str++;
+		}
+	}
+	return (words);
 }
