dc49c76352f0fddd94cd0b9350ef8cb96f3de74e
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-06 16:55:43 -0800

    My dog commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-*struct dog - a new type of data
-*representing a dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
+*struct dog - Start
+*@name: Specifies the name of dog
+*@age: Specifies the age of dog
+*@owner: Specifies owner's name
 */
 struct dog
 {
@@ -20,3 +21,12 @@ char *owner;
 *dog_t - Start
 */
 typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+#endif /* DOG_H */
