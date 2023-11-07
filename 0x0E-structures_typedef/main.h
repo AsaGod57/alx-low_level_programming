9245c4a5d78f5a4bd1383961b7b22c988dd6287a
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-06 16:32:58 -0800

    My first typedef commit

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - Start 
+*@name: Specifies the name of dog
+*@age: Specifies the age of dog
+*@owner: Specifies owners name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+/**
+*dog_t - Start
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+#endif /* DOG_H */
