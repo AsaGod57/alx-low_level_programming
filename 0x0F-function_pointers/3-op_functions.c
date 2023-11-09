adb49bc311b25ae3b54f48f6f76e777e155466f1
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-08 19:26:04 -0800

    My calc commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+*op_add - Start point
+*@a: Specifies the first number
+*@b: Speciifies the second number
+*Return:The sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+
+/**
+*op_sub - Start point
+*@a: Specifies the first number
+*@b: Specifies the second number
+*Return: The Difference of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul - Start point
+*@a: Specifies the first number
+*@b: Specifies the second number
+*Return: The multiplication of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+
+/**
+*op_div - Start point
+*@a: Specifies the first number
+*@b: Specifies the second number
+*Return: The quotient of a and b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+*op_mod - Start point
+*@a: Specifies the first number
+*@b: Specifies the second number
+*Return: The remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
