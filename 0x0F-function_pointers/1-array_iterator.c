9a196876d296be3cb112399dff880d970acf90dd
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-08 18:26:31 -0800

    My array commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+*array_iterator - Start point
+*@array: Specifies the array to be targeted
+*@size: Specifies the size of array
+*@action: Specifies the function to be executed
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+if (array == NULL || action == NULL)
+return;
+
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
+
+}
