5dcadbce1007d100060e7b6495d3c0be1298794a
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-21 19:56:02 -0800

    My reverse list commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - Starts to define function
+  * @head: Specifies description argument
+  * Return: The what does it return? (Ends)
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
