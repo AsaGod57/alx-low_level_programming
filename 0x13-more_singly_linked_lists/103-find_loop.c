6186b39989893b8f038019501350d2a6c37ab5f1
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-21 20:08:12 -0800

    My find loop commit

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * find_listint_loop - Starts to define function
+  * @head: Specifies head
+  * Return: The what does it return? (Ends)
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	head++;
+	return (0);
+}
