504a5fa9d0edb705542718da4817bd8225b4479b
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-21 20:00:36 -0800

    My print list safe commit

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe - Starts to define function
+  * @head: Specifies head
+  * Return: The what does it return? (Ends)
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
