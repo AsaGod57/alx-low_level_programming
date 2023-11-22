6519f427aaf20ac3c7a4aa52d3f2b867fec53246
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-21 20:04:47 -0800

    My free list safe commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * free_listint_safe - Starts to define function
+  * @h: Specifies head
+  * Return: The what does it return?
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	h++;
+	return (0);
+}
