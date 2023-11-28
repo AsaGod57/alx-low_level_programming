01f9a0b60a2eaf9ab09ed5e2b9cb2172a2f24f49
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-27 20:48:44 -0800

    My create file commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-  * create_file - Starts to create a file with rw------- permissions
+  * create_file - Starts to create a file with rw------- permits
   * @filename: Specifies name of the file, if NULL, return -1
-  * @text_content: Specifies contents of the file. If NULL, create an empty file
+  * @text_content: Specifies contents of the file. If NULL, create empty file
   * Return: 1 on success, -1 if otherwise (Ends)
   */
 int create_file(const char *filename, char *text_content)
