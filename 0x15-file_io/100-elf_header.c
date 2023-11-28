ce6a6bd2aad9c8ff2b5261e5bd88f1b03ad1fd29
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-27 20:45:17 -0800

    My header commit

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * placeholder2 - Specifies define function
+  * @void: Specifies description of argument
+  * Return: 0 on success (Ends)
+  */
+int placeholder2(void)
+{
+	return (0);
+}
