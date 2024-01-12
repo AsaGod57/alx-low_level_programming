64e0672a0b6c3e67a4c92f2c3d244765361085d3
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-11 17:43:55 -0800

    My insert dnodeint commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,53 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Starts to insert a new node at a given position
- * @h: Specifies pointer to head of list
+ * insert_dnodeint_at_index - Starts to  insert a new node at a given position
+ * @h: Specifies pointer to the head of linked list
  * @idx: Specifies index of the list where the new node should be added
- * @n: Specifies data of the new node
- * Return: The address of the new node, or NULL if  otherwise
+ * @n: Specifies value of the new node
+ * Return: The address of the new node, or NULL if otherwise
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node;
+	dlistint_t *head;
+	unsigned int i;
+
+	new_node = NULL;
+	if (idx == 0)	/* insert node at beginning of list */
+		new_node = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				/* insert note at the end of list */
+				if (head->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = head->next;
+						new_node->prev = head;
+						head->next->prev = new_node;
+						head->next = new_node;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
+	return (new_node);
 }
