d582764980969b103ab1425dfc0dbb70b4429987
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-15 17:22:22 -0800

    My abs commit

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _abs - Starts tocompute the absolute value of an integer
+* @n: Specifies the int to check
+* Return: The absolute value (Ends)
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
