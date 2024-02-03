d12cd9bfb84816133a70cae8e8bb48aab6b657f6
Godsway <gasamoah@wacci.ug.edu.gh>
2024-02-02 16:47:13 -0800

    My hash table commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-hash_table_create.c
+ * Auth: Godsway Asamoah
+ */
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -Starts to create a hash table
+ * @size: Specifies the size of the array
+ * Return: NULL when an error occurs (Ends)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
