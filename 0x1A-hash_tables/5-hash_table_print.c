a652a75660714c25b0f3dc00a8966980e16d51b6
Godsway <gasamoah@wacci.ug.edu.gh>
2024-02-03 09:42:31 -0800

    My hash table print commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+/*
+ * File: 5-hash_table_print.c
+ * Auth: Godsway Asamoah
+*/
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Starts to print a hash table
+ * @ht: Specifies a pointer to the hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
